58021c1fee4a3b957253938c18ae026346790983
Taufiq <taufiq9.2004@gmail.com>
2025-10-31 16:22:29 +0530

    auth http-server

--- apps/http-server/src/controllers/auth.ts
+++ apps/http-server/src/controllers/auth.ts
@@ -1,7 +1,36 @@
-export const signinHandler = ( ) => {
+import { prisma } from '@repo/db';
+import { Request, Response } from 'express';
+import { JWT_SECRET } from "@repo/common-backend/config"
+import jwt from "jsonwebtoken";
 
+export const signinHandler = async (req:Request, res:Response) => {
+    let user;
+    try {
+        user = await prisma.user.findOne({
+            data: req.body,
+        });
+    } catch (e) {
+        res.json({ error: 'user does not exists', e });
+        return;
+    }
+    const id = user.id;
+
+    const token = jwt.sign(id, JWT_SECRET);
+    res.json(token);
 }
 
-export const signupHandler = ( ) => {
+export const signupHandler = async (req:Request, res:Response) => {
+    let user;
+    try {
+        user = await prisma.user.create({
+            data: req.body,
+        });
+    } catch (e) {
+        res.json({ error: 'user already exists', e });
+        return;
+    }
+    const id = user.id;
 
+    const token = jwt.sign(id, JWT_SECRET);
+    res.json(token);
 }
