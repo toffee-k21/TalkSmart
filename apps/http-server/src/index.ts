444c98729f59d64375ae6a458753a548b4a1d1ac
Taufiq <taufiq9.2004@gmail.com>
2025-10-31 18:44:59 +0530

    cors fix

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from "express";
 import userRouter from "./route/auth";
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/auth", userRouter);
